0417c9ce3912a4312a54bccc228046d58b917090
Alex Naghtigall <alex@sd-design.tk>
2016-04-20 19:46:04 +0300

    Начал вникать в Angular JS

---
+++ angular/js/select.js
@@ -0,0 +1,11 @@
+angular.module('ngrepeatSelect', [])
+ .controller('ExampleController', ['$scope', function($scope) {
+   $scope.data = {
+    repeatSelect: null,
+    availableOptions: [
+      {id: 'success', name: 'success'},
+      {id: 'info', name: 'info'},
+      {id: 'danger', name: 'danger'}
+    ],
+   };
+}]);
