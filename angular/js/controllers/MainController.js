d53db17db09f867d142615dae437a79f0d736a13
Alex Solovey <naghtigall@gmail.com>
2017-07-18 15:47:03 +0300

    Изменения в 1.6.5
    Переписал контроллер в соответствии с версией Ангуляром 1.6.5.
    Добавил запрос по http к файлу json

--- angular/js/controllers/MainController.js
+++ angular/js/controllers/MainController.js
@@ -1,28 +1,8 @@
-app.controller('MainController', ['$scope', function($scope) {
-  $scope.apps = [ 
-	  { 
-	    icon: 'img/move.jpg', 
-	    title: 'MOVE', 
-	    developer: 'MOVE, Inc.', 
-	    price: 0.99 
-	  }, 
-	  { 
-	    icon: 'img/shutterbugg.jpg', 
-	    title: 'Shutterbugg', 
-	    developer: 'Chico Dusty', 
-	    price: 2.99 
-	  },
-	  {
-	    icon: 'img/gameboard.jpg',
-	    title: 'Gameboard',
-	    developer: 'Armando P.',
-	    price: 1.99
-	  },
-	  {
-	    icon: 'img/forecast.jpg',
-	    title: 'Forecast',
-	    developer: 'Forecast',
-	    price: 1.99
-	  }
-	];
-}]);
+app.controller('MainController', function($scope, $http) {
+
+	 $http.get("/angular/js/controllers/data.json").then(function(response) {
+         $scope.apps = response.data;
+
+    });
+
+});
